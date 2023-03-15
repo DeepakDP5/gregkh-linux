6d0b4089ca1b54ab9abc0bdba19d2da3796d3078
Deepak Patil <deppatil@amazon.com>
2023-03-15 11:35:53 +0530

    F1

--- scripts/make_kernel_shortlog.pl
+++ scripts/make_kernel_shortlog.pl
@@ -63,6 +63,7 @@ print "Changes in $major_version.$minor_version.$stable_version\n";
 print "Changes M1"
 print "Changes M2"
 print "Changes M3"
+print "Commit F1"
 
 open GIT, "git log --format=\"%s\" --reverse v$major_version.$minor_version.$last_version..v$major_version.$minor_version.$stable_version | " || die "Failed to run git";
 
